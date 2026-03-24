3221d26ce82e5b1911a562fb00eaa0082895886e
HiiPower05 <fordarkseid13@gmail.com>
2026-03-24 05:36:56 -0700

    Added a database models, User and Post. Defined their relationship. Tested by creating a user and post. Deleted the test. Add url_for().

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'look-over-your-shoulders, Honey'
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLACLCHEMY_TRACK_MODIFICATIONS = False
