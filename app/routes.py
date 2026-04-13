2145e47f90b242cc21495d80c1d6de0a22ddf71c
HiiPower05 <fordarkseid13@gmail.com>
2026-04-13 12:30:21 +0300

    Add some pagination. *fix posts logic

--- app/routes.py
+++ app/routes.py
@@ -7,24 +7,27 @@ from app.models import User
 from flask_login import logout_user, login_required
 from urllib.parse import urlparse
 from app.forms import EmptyForm
+from app.forms import PostForm
+from app.models import Post
 from app import app, db
 
+# susan - passwords
+# jon - ofthenorth
 
-@app.route("/")
-@app.route("/index")
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/index", methods=['GET', 'POST'])
 @login_required
 def index():
-    posts = [
-        {
-            'author': {'username': 'Lackluster'},
-            'body': 'Beautiful day in CPT!'
-        },
-        {
-            'author': {'username': 'Hwa Moogi'},
-            'body': 'Alliance not stopping me!'
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title='Home Page', form=form, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -131,3 +134,9 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+    
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
